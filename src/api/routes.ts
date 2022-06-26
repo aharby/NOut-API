9924631118bfdffd8c663831d5b48048b2369ada
bibo <abanoubharby@gmail.com>
2022-06-26 21:02:04 +0200

    'm'

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -12,7 +12,7 @@ import { registerErrorHandler, registerMiddleware } from './middleware';
 export function initRestRoutes(router: Router): void {
 	const prefix: string = '/api/v1';
 
-	router.get(prefix, (req: Request, res: Response) => res.send('PING'));
+	router.get(prefix, (req: Request, res: Response) => res.send('Welcome to NOut-API'));
 
 	registerMiddleware(router);
 	registerApiRoutes(router, prefix);
